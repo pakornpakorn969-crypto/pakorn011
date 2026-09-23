275eaaa4e422485339fffea1691806a922cbfb02
Pakorn <pakornpakorn969@gmail.com>
2026-09-23 14:09:38 +0700

    Fix indentation

--- main.py
+++ main.py
@@ -871,4 +871,4 @@ async def get_dashboard():
 # ==========================================
 
 if __name__ == "__main__":
-uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
